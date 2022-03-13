4a4b9ccc6557e6672558352ee376a850bccdcdee
Cristi Paris <cristian.tanasep@gmail.com>
2022-03-13 17:24:13 +0200

    Code refactoring

--- src/utils.h
+++ src/utils.h
@@ -4,19 +4,18 @@
 #ifndef __UTILS_H_
 #define __UTILS_H_
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* useful macro for handling error codes */
-#define DIE(assertion, call_description)				\
-	do {								\
-		if (assertion) {					\
-			fprintf(stderr, "(%s, %d): ",			\
-					__FILE__, __LINE__);		\
-			perror(call_description);			\
-			exit(errno);				        \
-		}							\
+#define DIE(assertion, call_description)                                       \
+	do {                                                                   \
+		if (assertion) {                                               \
+			fprintf(stderr, "(%s, %d): ", __FILE__, __LINE__);     \
+			perror(call_description);                              \
+			exit(errno);                                           \
+		}                                                              \
 	} while (0)
 
 #endif /* __UTILS_H_ */
