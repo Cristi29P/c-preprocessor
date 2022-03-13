edb013f134169eaf8ef63770883d9c896f0ae045
Cristi Paris <cristian.tanasep@gmail.com>
2022-03-13 18:32:19 +0200

    Added header file for hashmap

--- src/hashmap.h
+++ src/hashmap.h
@@ -1,12 +1,41 @@
-#pragma once
+#ifndef __HASHMAP_H__
+#define __HASHMAP_H__
 
-#include <stdlib.h>
-#include <string.h>
+#include "linkedlist.h"
 
+struct info {
+	void *key;
+	void *value;
+};
+
+struct Hashtable {
+	struct LinkedList *buckets;
+	int size;
+	int hmax;
+	unsigned int (*hash_function)(void *);
+	int (*compare_function)(void *, void *);
+};
+
+void init_ht(struct Hashtable *ht, int hmax,
+	     unsigned int (*hash_function)(void *),
+	     int (*compare_function)(void *, void *));
+
+void put(struct Hashtable *ht, void *key, int key_size_bytes, void *value);
+
+void *get(struct Hashtable *ht, void *key);
+
+int has_key(struct Hashtable *ht, void *key);
+
+void remove_ht_entry(struct Hashtable *ht, void *key);
+
+int get_ht_size(struct Hashtable *ht);
+
+int get_ht_hmax(struct Hashtable *ht);
+
+void free_ht(struct Hashtable *ht);
 
 int cmp_strings(void *a, void *b);
 
-/*
- * Credits: http://www.cse.yorku.ca/~oz/hash.html
- */
 unsigned int hash_function_string(void *a);
+
+#endif /* __HASHMAP_H__ */
