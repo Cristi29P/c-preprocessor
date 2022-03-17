6d610df4008458558608d0975265d2c6f0ae086e
Cristi Paris <cristian.tanasep@gmail.com>
2022-03-17 11:05:39 +0200

    Added cmd line parse functionality for define flags

--- src/so-cpp.c
+++ src/so-cpp.c
@@ -1,9 +1,65 @@
-#include "utils.h"
+#include "hashmap.h"
+// Refacut makefile si includes
+#define PATH_LENGTH 30
+#define SMALL_BUFF 20
+#define HT_ENTRIES 1000
 
+void add_cmd_define(struct Hashmap *mappings, char *argv)
+{
+	char symbol[SMALL_BUFF] = {'\0'}, value[SMALL_BUFF] = {'\0'};
+	sscanf(argv, "%[^=]=%s", symbol, value);
+	put(mappings, symbol, strlen(symbol) + 1, value, strlen(value) + 1);
+}
 
-int main(int argc, char *argv[]) {
-    
+void parse_cmd_defines(char *argv[], int argc, struct Hashmap *mappings)
+{
+	for (int i = 1; i < argc; i++) {
+		if (!strncmp(argv[i], "-D", 2) && strlen(argv[i]) == 2) {
+			i++; /*inaintez la urmatorul argument*/
+			add_cmd_define(mappings, argv[i]);
+		} else if (!strncmp(argv[i], "-D", 2) && strlen(argv[i]) > 2) {
+			add_cmd_define(mappings, argv[i] + 2);
+		}
+	}
+}
 
+void print_string_linkedlist(struct LinkedList *list)
+{
+	struct Node *curr;
+	curr = list->head;
 
-    return 0;
+	if (list == NULL) {
+		return;
+	}
+
+	while (curr != NULL) {
+		printf("%s ", (char *)(curr->data));
+		curr = curr->next;
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	char infile[PATH_LENGTH] = {'\0'}, outfile[PATH_LENGTH] = {'\0'};
+	struct LinkedList *directories =
+	    (struct LinkedList *)calloc(1, sizeof(struct LinkedList));
+	struct Hashmap *mappings =
+	    (struct Hashmap *)calloc(1, sizeof(struct Hashmap));
+	// Verificat si intors codul de eroare daca esueaza calloc-ul
+
+	init_list(directories);
+	init_ht(mappings, HT_ENTRIES, hash_function_string, cmp_strings);
+
+	parse_cmd_defines(argv, argc, mappings);
+	// print_string_linkedlist(directories);
+	printf("%d\n", get_ht_size(mappings));
+
+	char *ret = get(mappings, "TEST");
+	printf("Value: ---%s---\n", ret);
+
+	free_list(&directories);
+	free_ht(mappings);
+
+	return 0;
 }
