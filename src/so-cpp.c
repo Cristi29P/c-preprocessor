4e6b35a747845e0c2ed8aa92de6ee28cae886504
Cristi Paris <cristian.tanasep@gmail.com>
2022-03-17 13:47:27 +0200

    Added -I flag compatibility

--- src/so-cpp.c
+++ src/so-cpp.c
@@ -1,6 +1,6 @@
 #include "hashmap.h"
 // Refacut makefile si includes
-#define PATH_LENGTH 30
+#define PATH_LENGTH 50
 #define SMALL_BUFF 20
 #define HT_ENTRIES 1000
 
@@ -11,6 +11,12 @@ void add_cmd_define(struct Hashmap *mappings, char *argv)
 	put(mappings, symbol, strlen(symbol) + 1, value, strlen(value) + 1);
 }
 
+void add_cmd_directory(struct LinkedList *directories, char *argv) {
+	char directory[PATH_LENGTH] = {'\0'};
+	sscanf(argv, "%s", directory);
+	add_nth_node(directories, get_size(directories), directory, strlen(directory) + 1);
+}
+
 void parse_cmd_defines(char *argv[], int argc, struct Hashmap *mappings)
 {
 	for (int i = 1; i < argc; i++) {
@@ -19,6 +25,19 @@ void parse_cmd_defines(char *argv[], int argc, struct Hashmap *mappings)
 			add_cmd_define(mappings, argv[i]);
 		} else if (!strncmp(argv[i], "-D", 2) && strlen(argv[i]) > 2) {
 			add_cmd_define(mappings, argv[i] + 2);
+		}
+	}
+}
+
+void parse_cmd_directories(char *argv[], int argc,
+			   struct LinkedList *directories)
+{
+	for (int i = 1; i < argc; i++) {
+		if (!strncmp(argv[i], "-I", 2) && strlen(argv[i]) == 2) {
+			i++; /*inaintez la urmatorul argument*/
+			add_cmd_directory(directories, argv[i]);
+		} else if (!strncmp(argv[i], "-I", 2) && strlen(argv[i]) > 2) {
+			add_cmd_directory(directories, argv[i] + 2);
 		}
 	}
 }
@@ -52,11 +71,16 @@ int main(int argc, char *argv[])
 	init_ht(mappings, HT_ENTRIES, hash_function_string, cmp_strings);
 
 	parse_cmd_defines(argv, argc, mappings);
-	// print_string_linkedlist(directories);
-	printf("%d\n", get_ht_size(mappings));
+	parse_cmd_directories(argv, argc, directories);
+	
+	print_string_linkedlist(directories);
 
-	char *ret = get(mappings, "TEST");
+	char *ret = get(mappings, "DEBUG");
 	printf("Value: ---%s---\n", ret);
+
+	ret = get(mappings, "CUSTOM_DBG");
+	printf("Value: ---%s---\n", ret);
+
 
 	free_list(&directories);
 	free_ht(mappings);
